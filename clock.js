ef387b6e40b251ab02917dcbb70afda4d8539a31
kjs <koperson611@naver.com>
2019-12-18 19:03:19 +0900

    saveUserName

--- clock.js
+++ clock.js
@@ -1,16 +1,13 @@
 const clockContainer = document.querySelector(".js-clock"),
-clockTitle = clockContainer.querySelector(".js-title");
+clockTitle = clockContainer.querySelector("h1");
 
 function getTime(){
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${
-        hours < 10 ? `0${hours}` : hours
-    }:${
-        minutes < 10 ? `0${minutes}` : minutes
-    }:${
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours
+    }:${minutes < 10 ? `0${minutes}` : minutes}:${
         seconds < 10 ? `0${seconds}`:seconds
     }`;
 }
