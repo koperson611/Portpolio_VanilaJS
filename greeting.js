70d931eb2f632e1b6da79b2adcab9c753a1b64bb
kjs <koperson611@naver.com>
2019-12-18 19:12:20 +0900

    SaveUserName

--- greeting.js
+++ greeting.js
@@ -8,21 +8,25 @@ const USER_LS = "currentUser",
 function saveName(text){
     localStorage.setItem(USER_LS, text);
 }
+
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = input.value;
     paintGreeting(currentValue);
     saveName(currentValue);
 }
+
 function askForName(){
     form.classList.add(SHOWING_CN);
     form.addEventListener("submit",handleSubmit);
 }
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
+
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS)
     if(currentUser === null){
